f2f49aa56426658f716be62a024eef0e028735af
Daniel Gonçalves <dn648979@gmail.com>
2025-07-31 21:33:49 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/components/CadastroGeral.jsx
+import React, { useState } from 'react';
+import CadastroGeral from "./Components/CadastroGeral/CadastroGeral";
+
+export default function App(){
+  return(
+    <>
+
+      <CadastroGeral />
+    </>
+  )
+}
